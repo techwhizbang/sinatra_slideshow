299bbe54559a0d91fa6c4d11941b54e4f26ed14c
nz <techwhizbang@gmail.com>
2010-08-07 10:41:22 -0700

    nz: moving code to separate repo

--- sinatra_code/app/views/standalone/show.builder
+++
@@ -1,4 +0,0 @@
-xml.instruct! :xml, :version=>"1.0", :encoding=>"UTF-8"
-xml.sinatra do
-  xml.rocks('Ridiculously Simple, well sort of...(the automagic xml variable is not documented)')
-end
--- sinatra_code/spec/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-require 'spec'
-require 'spec/autorun'
-#Don't forget to include this to use Rack::Test
-require 'rack/test'
-#Load your application files
-require File.expand_path(File.dirname(__FILE__) + "/../init")
-
